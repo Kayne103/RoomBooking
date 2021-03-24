7f5bf935a85476a12f2c725f8c49a92306c4e30f
Kayne <holykayne@gmail.com>
2021-03-24 19:46:35 +0200

    changed database ip.

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ Database configurations
 app.config['MYSQL_DATABASE_USER'] = 'hope'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'hope'
 app.config['MYSQL_DATABASE_DB'] = 'roombooking'
-app.config['MYSQL_DATABASE_HOST'] = '40.123.249.179'
+app.config['MYSQL_DATABASE_HOST'] = '10.0.38.138'
 
 mysql.init_app(app)
